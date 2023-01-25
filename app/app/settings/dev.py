b6a053b32c827914778f4404d12482bf0d10a5b8
bedirhansahin <bedirhannsahin@gmail.com>
2023-01-25 12:09:54 +0300

    first commit

---
+++ app/app/settings/dev.py
@@ -0,0 +1,10 @@
+from .base import *
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
