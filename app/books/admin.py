4a45c1c6e64e826c16bc27aa93e345d5348f16d4
bedirhansahin <bedirhannsahin@gmail.com>
2023-02-01 16:17:55 +0300

    books and authors models are created and admin page is configured

--- app/books/admin.py
+++ app/books/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Author, Book, Category
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+    list_display = ['name', 'author']
+    ordering = ['name']
+    search_fields = ['name', 'author', 'category']
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+    list_display = ['name', 'country']
+    ordering = ['name']
+    search_fields = ['name', 'books']
--- app/books/models.py
+++ app/books/models.py
@@ -1,3 +1,39 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, null=True)
+    slug = models.SlugField(max_length=100, unique=True, null=True)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+
+class Author(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, null=True)
+    slug = models.SlugField(max_length=100, unique=True, null=True)
+    date_of_birth = models.DateField()
+    country = models.CharField(max_length=255)
+    books = models.ManyToManyField('Book', related_name='books', null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, null=True)
+    slug = models.SlugField(max_length=100, unique=True, null=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    category = models.ManyToManyField(Category)
+    summary = models.TextField()
+
+    def __str__(self):
+        return self.name
